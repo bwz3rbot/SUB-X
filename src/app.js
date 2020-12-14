de1cb7deaf6528725d0a6c3bccbc8b8fa2ac0d68
customwebtemplatedesigner <68409068+customwebtemplatedesigner@users.noreply.github.com>
2020-12-14 11:59:33 -0700

    heroku

--- src/app.js
+++ src/app.js
@@ -9,29 +9,35 @@ const VOCHALS_SUBS = process.env.VOCHALS_SUBS.split(/[ ,]+/);
 const DRUMS_SUBS = process.env.DRUMS_SUBS.split(/[ ,]+/);
 const THEORY_SUBS = process.env.THEORY_SUBS.split(/[ ,]+/);
 
-console.log({GUITAR_SUBS,BASS_SUBS,VOCHALS_SUBS,DRUMS_SUBS,THEORY_SUBS});
+console.log({
+    GUITAR_SUBS,
+    BASS_SUBS,
+    VOCHALS_SUBS,
+    DRUMS_SUBS,
+    THEORY_SUBS
+});
 
 const Snoolicious = require('./lib/Snoolicious');
 const snoolicious = new Snoolicious();
 
 async function handleSubmission(task) {
     const FLAIRTEXT = task.item.link_flair_text;
-    switch(FLAIRTEXT){
-        case(process.env.GUITAR_FLAIR):
-        await xpost(task.item,GUITAR_SUBS);
-        break;
-        case(process.env.BASS_FLAIR):
-        await xpost(task.item,BASS_SUBS);
-        break;
-        case(process.env.VOCHALS_FLAIR):
-        await xpost(task.item,VOCHALS_SUBS);
-        break;
-        case(process.env.DRUMS_FLAIR):
-        await xpost(task.item,DRUMS_SUBS);
-        break;
-        case(process.env.THEORY_FLAIR):
-        await xpost(task.item,THEORY_SUBS);
-        break;
+    switch (FLAIRTEXT) {
+        case (process.env.GUITAR_FLAIR):
+            await xpost(task.item, GUITAR_SUBS);
+            break;
+        case (process.env.BASS_FLAIR):
+            await xpost(task.item, BASS_SUBS);
+            break;
+        case (process.env.VOCHALS_FLAIR):
+            await xpost(task.item, VOCHALS_SUBS);
+            break;
+        case (process.env.DRUMS_FLAIR):
+            await xpost(task.item, DRUMS_SUBS);
+            break;
+        case (process.env.THEORY_FLAIR):
+            await xpost(task.item, THEORY_SUBS);
+            break;
     }
 
 }
@@ -41,7 +47,12 @@ async function xpost(post, subs) {
     console.log(post.title);
     for (const sub of subs) {
         console.log(`X-Posting to r/${sub}...`.grey);
-        await snoolicious.requester._newObject('Submission', post, true).submitCrosspost({
+        if (!await snoolicious.requester.getSubreddit(sub).user_is_subscriber) {
+            console.log(`Was not already subscribed to r/${sub}. Subscribing now...`);
+            await snoolicious.requester.getSubreddit(sub).subscribe();
+        }
+        await snoolicious.requester.
+        _newObject('Submission', post, true).submitCrosspost({
             subredditName: sub,
             title: post.title,
             originalPost: this,
